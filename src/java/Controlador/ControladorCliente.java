f6838a020d068346ee5a4165add0fffdafe73c25
ArgledZ <gcamo@hotmail.com>
2022-06-21 23:12:12 -0500

    v1

---
+++ src/java/Controlador/ControladorCliente.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import DAO.ClienteDAO;
+import DAO.TipodocumentoDAO;
+import Entidades.Cliente;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author gcamo
+ */
+@WebServlet(name = "ControladorCliente", urlPatterns = {"/ControladorCliente"})
+public class ControladorCliente extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    private HttpSession sesion;
+    Cliente cl = new Cliente();
+    ClienteDAO cldao = new ClienteDAO();
+    TipodocumentoDAO tpdoc = new TipodocumentoDAO();
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String menu = request.getParameter("menu");
+        String accion = request.getParameter("accion");
+
+        if (menu.equals("Principal")) {
+            response.sendRedirect("Principal.jsp");
+
+        }
+        if(menu.equals("Clientes")){
+            sesion = request.getSession();
+                    sesion.removeAttribute("listaClientes");
+                    sesion.removeAttribute("listaEmpleado");
+                    List lista = cldao.listarClientes();
+                    sesion.setAttribute("listaClientes", lista);
+                    
+                    List listatd = tpdoc.listardocumentos();
+                    sesion.setAttribute("listadocumentos", listatd);
+                    /*
+                    List listacar = cargo.listarCargos();
+                    sesion.setAttribute("listacargos", listacar);
+
+                    List listaestado = estado.listarEstados();
+                    sesion.setAttribute("listaestado", listaestado);
+                    */
+        response.sendRedirect("Cliente.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/DAO/ClienteDAO.java
@@ -0,0 +1,297 @@
+package DAO;
+
+import config.Conexion;
+import Entidades.Cliente;
+import Entidades.Tipodocumento;
+import static java.lang.Integer.parseInt;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ClienteDAO {
+
+        public List listarClientes(){
+            EmpleadoDAO cldao = new EmpleadoDAO();
+            List lista=null;       
+            return lista;
+        }
+        /*public List<Cliente> listarClientes(){
+        Connection con=null;
+        CallableStatement cstm = null;
+        ResultSet rs=null;
+         List<Cliente> list=null;
+          
+       
+        try {
+            con=Conexion.getConection();
+           
+           cstm = con.prepareCall("{ SELECT P.id_persona,P.Nom_persona,P.Ape_persona,P.Id_tipo_documento,P.numero_identificacion,P.edad_persona,P.Nacionalidad_persona,P.Empresa_persona,P.Correo_persona,P.Telf_persona,P.Fecha_naci_persona,P.Dir_persona,P.Sex_persona, C.Fec_regis_cliente,C.Puntos,TD.Des_tipo_documento FROM cliente C INNER JOIN persona P ON C.Id_persona=P.Id_persona INNER JOIN tipo_documento TD ON P.Id_tipo_documento=TD.Id_tipo_documento WHERE P.eliminado=0;} ");
+             
+            list=new ArrayList<>();
+            Cliente em=null;
+            rs = cstm.executeQuery();
+            
+            while(rs.next()){
+                em =new Cliente();
+                em.setId_persona(parseInt(rs.getString("Id_persona")));
+                em.setNom_persona(rs.getString("Nom_persona"));
+                em.setApe_persona(rs.getString("Ape_persona"));
+                em.setTipodocumento(new Tipodocumento(rs.getInt("Id_tipo_documento")));
+                em.setTipodocumento(new Tipodocumento(rs.getInt("Id_tipo_documento"),rs.getString("Des_tipo_documento")));
+                em.setNumero_identicacion(parseInt(rs.getString("Numero_identificacion")));
+                em.setEdad_persona(parseInt(rs.getString("Edad_persona")));
+                em.setNacioalidad_persona(rs.getString("Nacionalidad_persona"));
+                em.setEmpresa_persona(rs.getString("Empresa_persona"));
+                em.setCorreo_persona(rs.getString("Correo_persona"));
+                em.setTelf_persona(rs.getString("Telf_persona"));
+                em.setDir_persona(rs.getString("Dir_persona"));
+                em.setSex_persona(rs.getString("Sex_persona"));
+                em.setFec_regis_cliente(rs.getDate("Fec_regis_cliente"));
+                em.setPuntos(rs.getInt("puntos"));
+                
+                list.add(em);
+            }
+            return list;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally{
+            try {
+               if(rs!=null) rs.close();
+               if(cstm!=null) cstm.close();
+               
+               
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }  
+        }    
+        return list;
+    }*/
+        
+    //INSERTAR
+        
+       public boolean insertar(Cliente cliente){
+        
+       boolean confirmacion2=false;
+          Connection con=null;
+        CallableStatement cstm = null;
+        
+       
+         try {
+     
+          
+           con=Conexion.getConection();
+           cstm = con.prepareCall("{CALL painsertarcliente(?,?,?,?,?,?,?,?,?,?,?)}");
+           cstm.setString(1, cliente.getNom_persona());
+           cstm.setString(2, cliente.getApe_persona());
+           cstm.setInt(3,cliente.getTipodocumento().getId_tipo_documento());
+           cstm.setInt(4,cliente.getNumero_identicacion());
+           cstm.setInt(5,cliente.getEdad_persona());
+           cstm.setString(6,cliente.getNacioalidad_persona());
+           cstm.setString(7,cliente.getEmpresa_persona());
+           cstm.setString(8,cliente.getCorreo_persona());
+           cstm.setString(9,cliente.getTelf_persona());
+           cstm.setString(10,cliente.getDir_persona());
+           cstm.setString(11,cliente.getSex_persona());
+           
+         
+          
+           
+           confirmacion2 = cstm.execute();
+            
+          
+            
+        } catch (Exception ex) {
+            
+            System.out.println(ex.getMessage());
+            
+        }finally{
+         
+             try {
+                 
+                  if(cstm!=null) cstm.close();
+                  
+                                 
+             } catch (Exception e) {
+                 System.out.println(e.getMessage());
+                 
+             }
+            
+         
+         }
+        return true;
+   
+    }     
+        
+        
+        
+        
+  //BUSCAREMPLEADO
+        
+       public List<Cliente> buscarCliente(String clienteado){
+        List<Cliente> list=null;
+        Connection con=null;
+        CallableStatement cstm = null;
+        ResultSet rs=null;
+        try {
+            //con=Conexion.getConexion();
+            //String sql="select * from cliente where Apellidos like '"+cliente+"%'";
+            
+            //pstm=con.prepareStatement(sql);
+            //rs=pstm.executeQuery();
+            
+            //list=new ArrayList<>();
+            //Cliente client=null;
+           
+            con=Conexion.getConection();
+           
+           cstm = con.prepareCall("{ call pabuscarcliente(?)} ");
+           cstm.setString(1, clienteado);
+           rs = cstm.executeQuery(); 
+           
+            list=new ArrayList<>();
+            Cliente em=null;
+           
+            
+            
+            while(rs.next()){
+                em=new Cliente();
+               em =new Cliente();
+                em.setId_persona(rs.getInt("Id_persona"));
+                em.setNom_persona(rs.getString("Nom_persona"));
+                em.setApe_persona(rs.getString("Ape_persona"));
+                em.setTipodocumento(new Tipodocumento(rs.getInt("Id_tipo_documento")));
+                em.setTipodocumento(new Tipodocumento(rs.getInt("id_tipo_documento"),rs.getString("Des_tipo_documento")));
+                em.setNumero_identicacion(parseInt(rs.getString("Numero_identificacion")));
+                em.setEdad_persona(parseInt(rs.getString("Edad_persona")));
+                em.setNacioalidad_persona(rs.getString("Nacionalidad_persona"));
+                em.setEmpresa_persona(rs.getString("Empresa_persona"));
+                em.setCorreo_persona(rs.getString("Correo_persona"));
+                em.setTelf_persona(rs.getString("Telf_persona"));
+                em.setDir_persona(rs.getString("Dir_persona"));
+                em.setSex_persona(rs.getString("Sex_persona"));
+                em.setFec_regis_cliente(rs.getDate("Fec_regis_cliente"));
+                em.setPuntos(rs.getInt("puntos"));
+                
+                list.add(em);
+            }
+         
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally{
+            try {
+               if(rs!=null) rs.close();
+               if(cstm!=null) cstm.close();
+               if(con!=null) con.close();
+               
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }  
+        }    
+        return list;
+    } 
+        
+       
+       
+    //MODIFICAR EMPLEADO
+       
+         public boolean actualiza(Cliente cliente){
+        
+      
+          Connection con=null;
+        CallableStatement cstm = null;
+        
+       
+         try {
+          
+          
+           con=Conexion.getConection();
+           cstm = con.prepareCall("{CALL pamodificarcliente(?,?,?,?,?,?,?,?,?,?,?,?)}");
+           cstm.setInt(1, cliente.getId_persona());
+           cstm.setString(2, cliente.getNom_persona());
+           cstm.setString(3, cliente.getApe_persona());
+           cstm.setInt(4,cliente.getTipodocumento().getId_tipo_documento());
+           cstm.setInt(5,cliente.getNumero_identicacion());
+           cstm.setInt(6,cliente.getEdad_persona());
+           cstm.setString(7,cliente.getNacioalidad_persona());
+           cstm.setString(8,cliente.getEmpresa_persona());
+           cstm.setString(9,cliente.getCorreo_persona());
+           cstm.setString(10,cliente.getTelf_persona());
+           cstm.setString(11,cliente.getDir_persona());
+           cstm.setString(12,cliente.getSex_persona());
+          
+           
+           cstm.executeUpdate();
+            
+          
+            
+               
+        } catch (Exception ex) {
+            
+            System.out.println(ex.getMessage());
+            
+        }finally{
+         
+             try {
+                 
+                  if(cstm!=null) cstm.close();
+                  
+                                 
+             } catch (Exception e) {
+                 System.out.println(e.getMessage());
+                 
+             }
+            
+         
+         }
+        return true;
+   
+    }    
+       
+       
+   //ELIMINAR EMPLEADO
+       
+     public boolean Eliminar(Cliente cliente){
+        
+       boolean confirmacion=false;
+          Connection con=null;
+        CallableStatement cstm = null;
+        
+       
+         try {
+          
+          
+            con=Conexion.getConection();
+           cstm = con.prepareCall("{ call paeliminarpersona(?,?)} ");
+           cstm.setInt(1, cliente.getId_persona());
+           cstm.setInt(2, 1);
+           confirmacion = cstm.execute();
+            
+          
+            
+        } catch (Exception ex) {
+            
+            System.out.println(ex.getMessage());
+            
+        }finally{
+         
+             try {
+                 
+                  if(cstm!=null) cstm.close();
+                  
+                                 
+             } catch (Exception e) {
+                 System.out.println(e.getMessage());
+                 
+             }
+            
+         
+         }
+        return true;
+   
+    } 
+
+}
